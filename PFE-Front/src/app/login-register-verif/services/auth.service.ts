a4d7a90a0a10034339736549f8ec540dabb83a4f
LtNightFury <omar.ajroud13@gmail.com>
2025-04-03 12:57:25 +0100

    committed

--- PFE-Front/src/app/login-register-verif/services/auth.service.ts
+++ PFE-Front/src/app/login-register-verif/services/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
